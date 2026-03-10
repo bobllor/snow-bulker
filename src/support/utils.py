20929c41c0d31873dc8aeb31ae0b482be24bdbac
bobllor <bobllor@yahoo.com>
2026-03-10 17:18:55 -0500

    fixed an issue with format_pid and added docs

--- src/support/utils.py
+++ src/support/utils.py
@@ -27,6 +27,12 @@ def get_config(root: Path, file: Path | str, yaml_extensions: list[str], config_
     return config
 
 def format_pid(pid: str) -> str:
+    '''Formats the PID for missing leading zeroes. If the PID is not a number,
+    then it will do nothing to the string.
+    '''
+    if pid.isalpha():
+        return pid
+
     LEAD_ZEROES_COUNT: int = 4
     zeroes_found: int = 0
 
